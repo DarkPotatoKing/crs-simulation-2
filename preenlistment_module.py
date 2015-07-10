f7df0b8decb74f49601195327f852f0616f06d6b
Kyle Rosales <kyle.rosales97@gmail.com>
2015-07-11 00:17:45 +0800

    added conflict checking between classes

--- preenlistment_module.py
+++ preenlistment_module.py
@@ -24,5 +24,25 @@ class PreenlistmentModule:
 
     @staticmethod
     def has_conflict(course1, course2):
-        pass
-        
+        # check if they are the same subject or overlapping schedule
+        return is_same_subject(course1, course2) or is_overlapping_schedule(course1, course2)
+
+    @staticmethod
+    def is_same_subject(course1, course2):
+        return course1.subject == course2.subject
+
+    @staticmethod
+    def is_overlapping_schedule(course1, course2):
+        # check if they are the same section
+        if course1.section == course2.section:
+            return True
+
+        # check overlapping schedules using conflict_sections attribute
+        # iterate through conflict_sections
+        for x in course1.conflict_sections:
+            # check if x has a substring in course2's section
+            if course2.section.find(x) != -1:
+                return True
+
+        # if the none of the above conditions are satisfied, return False
+        return False
