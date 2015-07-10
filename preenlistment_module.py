34fa7ccbc014d0c162db090602031bb0d3dc7c32
Kyle Rosales <kyle.rosales97@gmail.com>
2015-07-11 02:02:28 +0800

    added batchrun() method

--- preenlistment_module.py
+++ preenlistment_module.py
@@ -1,4 +1,6 @@
+from __future__ import division
 from course import Course
+import random
 
 class PreenlistmentModule:
     """docstring for PreenlistmentModule"""
@@ -14,13 +16,51 @@ class PreenlistmentModule:
                 self.add_class(*args)
 
     def batchrun(self, num_runs = 1):
-        pass
+
+        for run_num in xrange(num_runs):
+            granted_classes = list()
+            units = 0
+            print '\nRun #%s' % str(run_num+1)
+            print 'Granted classes:\n'
+
+            # iterate through desired classes
+            for x in self.desired_classes:
+                # randomize slot
+                if PreenlistmentModule.class_granted(x):
+                    # if granted, check for conflict
+                    conflict = False
+                    for y in granted_classes:
+                        conflict = PreenlistmentModule.has_conflict(x,y)
+                        # break if there is conflict
+                        if conflict:
+                            break
+
+                    # check if adding subeject results to overload
+                    if units + x.credits > 20.0:
+                        conflict = True
+
+                    # add class only if no conflict
+                    if not conflict:
+                        granted_classes.append(x)
+                        units += x.credits
+
+            # print granted classes
+            for x in granted_classes:
+                print '{}\t{}\t({})'.format(x.subject, x.section, x.credits)
+            print 'Total units: {}'.format(units) 
+
+
 
     def add_class(self, *args):
         self.desired_classes.append(Course(*args))
 
     def __repr__(self):
         return str(self.desired_classes)
+
+    @staticmethod
+    def class_granted(course):
+        random.seed()
+        return random.random() < course.available_slots / course.demand
 
     @staticmethod
     def is_same_subject(course1, course2):
